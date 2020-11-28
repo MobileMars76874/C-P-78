a2db4cec029ef1982362e14baaf49aa7757a8579
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2020-11-28 21:24:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var Name = ["Sanjeev Garg" , "Hema Garg" , "Nitai Garg",]
+var Image = ["Father.jpg","Mother.jpg","Nitai Picture.jpg"]
+
+
+var i = 0;
+
+
+function Nextslide(){
+
+document.getElementById("name").innerHTML=Name[i];
+document.getElementById("image").src=Image[i];
+i++;
+
+if(i >= 3){
+i = 0
+}
+
+}
